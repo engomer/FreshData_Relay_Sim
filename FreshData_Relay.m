08bf7545fe38fdad4e415895b4495783b1582970
ratatosk <ratatosk@willywonka>
2022-09-16 14:31:13 +0300

    sc

--- FreshData_Relay.m
+++ FreshData_Relay.m
@@ -12,29 +12,29 @@ queueLength = 25; %queue length
 RelayNodeCapacity = 7; %relay node capacity
 t = 0.1:0.1:Days*24*60*60;
 
-Node_Age = randi(390,numOfNodes,1);
-
+NodetoRelay_Age = randi(600,numOfNodes,1);
+RelaytoSat_Age = NodetoRelay_Age;
 Node_Priority = randi(8,numOfNodes,1) + 1;
 QueueArray = zeros(numOfNodes,queueLength);
 channel_Sel = randi(7,numOfNodes,1) + 1;
 numOfTx = zeros(numOfNodes,1);
 numOfRelayTx = zeros(numOfNodes,1);
 totalRelaytoSat_Age = zeros(numOfNodes, Days*satPassNum + 1);
-totalRelaytoSat_Age(:,1) = Node_Age;
+totalRelaytoSat_Age(:,1) = NodetoRelay_Age;
 
 %% DUMM Node Case
 oneDayt = 24*60*60;
 txNum = 1;
 for ii = 1:length(t)
     
-    mayTransmitIdx = mod(Node_Age,65) == 0;
+    mayTransmitIdx = mod(NodetoRelay_Age,65) == 0;
     mayTransmit = find(mayTransmitIdx==1);
     mayTransmitSize = size(mayTransmit);
     
     [~,canTransmitIdx] = unique(channel_Sel(mayTransmit));
     canTransmit = mayTransmit(canTransmitIdx);
     
-    Node_Age(canTransmit) = 0;
+    NodetoRelay_Age(canTransmit) = 0;
     numOfTx(canTransmit) = numOfTx(canTransmit) + 1;
     
     channel_Sel(mayTransmit) = randi(7,mayTransmitSize(1),1) + 1;
@@ -54,10 +54,11 @@ for ii = 1:length(t)
         QueueArray(RelayTxIdx,:) = circshift(QueueArray(RelayTxIdx,:),-1,2);
         numOfRelayTx(RelayTxIdx,1) = numOfRelayTx(RelayTxIdx,1) + 1;
         totalRelaytoSat_Age(RelayTxIdx,txNum) = RelaytoSat_Age(RelayTxIdx,1);
+        RelaytoSat_Age(RelayTxIdx) = 0;
         
     end
     QueueArray(QueueArray~=0) = QueueArray(QueueArray~=0) + 0.1;
-    Node_Age = Node_Age + 1;
+    NodetoRelay_Age = NodetoRelay_Age + 1;
     RelaytoSat_Age = RelaytoSat_Age + 1;
 end
 
