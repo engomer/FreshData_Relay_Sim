cabf6df6257aed174f9b4da18c543720f408b44c
ratatosk <ratatosk@willywonka>
2022-09-16 13:32:22 +0300

    second

--- FreshData_Relay.m
+++ FreshData_Relay.m
@@ -4,37 +4,37 @@ clear;clc;
 format long
 tic
 
-numberOfPasses = [2 4 10 15];
-satPassNum = numberOfPasses(1);
-Days = 1;
-numOfNodes = 100;
-queueLength = 25;
-RelayNodeCapacity = 7;
+numberOfPasses = [2 4 10 15]; %number of satellite passes
+satPassNum = numberOfPasses(1); %selected sat pass case
+Days = 1; %simulation day duration
+numOfNodes = 100; %number of nodes
+queueLength = 25; %queue length
+RelayNodeCapacity = 7; %relay node capacity
 t = 0.1:0.1:Days*24*60*60;
 
-NodetoRelay_Age = randi(3600,numOfNodes,1);
-RelaytoSat_Age = NodetoRelay_Age;
+Node_Age = randi(390,numOfNodes,1);
+
 Node_Priority = randi(8,numOfNodes,1) + 1;
 QueueArray = zeros(numOfNodes,queueLength);
 channel_Sel = randi(7,numOfNodes,1) + 1;
 numOfTx = zeros(numOfNodes,1);
 numOfRelayTx = zeros(numOfNodes,1);
 totalRelaytoSat_Age = zeros(numOfNodes, Days*satPassNum + 1);
-totalRelaytoSat_Age(:,1) = NodetoRelay_Age;
+totalRelaytoSat_Age(:,1) = Node_Age;
 
 %% DUMM Node Case
 oneDayt = 24*60*60;
 txNum = 1;
 for ii = 1:length(t)
     
-    mayTransmitIdx = mod(NodetoRelay_Age,65) == 0;
+    mayTransmitIdx = mod(Node_Age,65) == 0;
     mayTransmit = find(mayTransmitIdx==1);
     mayTransmitSize = size(mayTransmit);
     
     [~,canTransmitIdx] = unique(channel_Sel(mayTransmit));
     canTransmit = mayTransmit(canTransmitIdx);
     
-    NodetoRelay_Age(canTransmit) = 0;
+    Node_Age(canTransmit) = 0;
     numOfTx(canTransmit) = numOfTx(canTransmit) + 1;
     
     channel_Sel(mayTransmit) = randi(7,mayTransmitSize(1),1) + 1;
@@ -54,11 +54,10 @@ for ii = 1:length(t)
         QueueArray(RelayTxIdx,:) = circshift(QueueArray(RelayTxIdx,:),-1,2);
         numOfRelayTx(RelayTxIdx,1) = numOfRelayTx(RelayTxIdx,1) + 1;
         totalRelaytoSat_Age(RelayTxIdx,txNum) = RelaytoSat_Age(RelayTxIdx,1);
-        RelaytoSat_Age(RelayTxIdx) = 0;
         
     end
     QueueArray(QueueArray~=0) = QueueArray(QueueArray~=0) + 0.1;
-    NodetoRelay_Age = NodetoRelay_Age + 1;
+    Node_Age = Node_Age + 1;
     RelaytoSat_Age = RelaytoSat_Age + 1;
 end
 
