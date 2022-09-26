822c13fe1599fff04fafbbf5bb143a92eaf78411
ratatoskr <ogencay98@gmail.com>
2022-09-26 17:52:13 +0300

    heterogen update

--- FreshData_Relay.m
+++ FreshData_Relay.m
@@ -3,11 +3,12 @@
 
 format long
 
-
-% numberOfPasses = [2 4 10 15]; %number of satellite passes
-% satPassNum = numberOfPasses(1); %selected sat pass case
-% Days = 1; %simulation day duration
-% numOfNodes = 10; %number of nodes
+% caseNum = 1;
+% numberOfPasses = [4 12 24]; %number of satellite passes
+% satPassNum = numberOfPasses(caseNum); %selected sat pass case
+% Days = 5; %simulation day duration
+% Nodes = [19 51 99];
+% numOfNodes = Nodes(caseNum); %number of nodes
 % queueLength = 25; %queue length
 % RelayNodeCapacity = 7; %relay node capacity
 % t = 0.1:0.1:Days*24*60*60;
@@ -25,13 +26,16 @@ numOfTx = zeros(numOfNodes,1);
 numOfRelayTx = zeros(numOfNodes,1);
 total_Age = zeros(numOfNodes, Days*satPassNum + 1);
 total_Age(:,1) = Node_Age;
+mayTransmitIdx = zeros(numOfNodes,1);
 
 %% DUMB Node Case
 oneDayt = 24*60*60;
 txNum = 1;
 for ii = 1:length(t)
     
-    mayTransmitIdx = mod(Node_Age,300) == 0;
+    mayTransmitIdx = mod(Node_Age,length(t)/Days) == 0;
+    mayTransmitIdx(1:3,:) = 0;
+    mayTransmitIdx(1:3,:) = mod(Node_Age(1:3,:),length(t)/(Days*satPassNum)) == 0;
     mayTransmit = find(mayTransmitIdx==1);
     mayTransmitSize = size(mayTransmit);
     
@@ -56,23 +60,22 @@ for ii = 1:length(t)
     AgeArray(:,ii) = Age;
     Age = Age + 0.1;
 
-    if mod(ii,ceil(Days*length(t)/(Days*satPassNum)))==0
+    if mod(ii,ceil(length(t)/(Days*satPassNum)))==0
         txNum = txNum+1;
-        QueueLCArray = sortrows([QueueArray(:,1), (1:numOfNodes)'],-1);
-        RelayTxIdx = QueueLCArray(1:RelayNodeCapacity,2);
-
-        numOfRelayTx(RelayTxIdx,1) = numOfRelayTx(RelayTxIdx,1) + 1;
+        QueueLCArray = sortrows([QueueArray(4:end,1), (4:numOfNodes)'],-1);
         
+        RelayTxIdx = [1;2;3;QueueLCArray(QueueLCArray(1:RelayNodeCapacity-3,1)~= 0,2);];
+        
+        numOfRelayTx(RelayTxIdx,1) = numOfRelayTx(RelayTxIdx,1) + 1;
+    
         total_Age(RelayTxIdx,txNum) = Age(RelayTxIdx,1);
         Age(RelayTxIdx) = QueueArray(RelayTxIdx,1)-Priority_Weight*Node_Priority(RelayTxIdx,1);
-        
+    
         QueueArray(RelayTxIdx,1) = 0;
         QueueArray(RelayTxIdx,:) = circshift(QueueArray(RelayTxIdx,:),-1,2);
         
     end
 end
-minmeanPeakAge_PerNode = min(max(AgeArray,[],2));
-peakAge = max(AgeArray,[],"all");
-meanNodeAge = mean(AgeArray,2);
-meanAge = mean(AgeArray,"all");
+
+PeakAge_PerNode = max(AgeArray,[],2);
 
